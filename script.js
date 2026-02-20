e35a27d6ef8e57f1bf9a04d48fd2a4821c2d2430
Jagjotsingh00 <iamjagjot02@gmail.com>
2026-02-20 21:33:02 +0530

    5 tasks done

--- script.js
+++ script.js
@@ -15,9 +15,50 @@ btn2.addEventListener("click", () => {
 });
 
 // -------------------------------------------------------
-const btn3 = document.querySelector("btn-3")
+const btn3 = document.querySelector("#btn-3")
 const userInput = document.getElementById("user-input")
 
 btn3.addEventListener("click" , () => {
-    userInput = alert(userInput.value)
+if(userInput.value === "") {
+    alert("Please enter text..")
+    return;
+}else{
+    alert(userInput.value)
+}
 }) 
+
+// --------------------------------------------------------
+const btn4 = document.querySelector("#btn-4")
+const link = document.querySelector("#my-link")
+
+btn4.addEventListener("click" , () => {
+    link.innerHTML = "<a href= https://youtube.com > Go to YouTube </a>"
+})
+
+// --------------------------------------------------------
+
+const btn5 = document.querySelector("#btn-5")
+const myImage = document.querySelector("#my-image")
+
+btn5.addEventListener("click" , () => {
+    myImage.style.display = "none"
+})
+
+// --------------------------------------------------------
+
+    const incbtn = document.querySelector("#inc-btn")
+    const decbtn = document.querySelector("#dec-btn")
+    const counterValue = document.querySelector("#counter-value")
+
+// incbtn.addEventListener("click" , () => {
+//  const Value = counterValue.value++
+// })
+
+// ----------------------------------------------------------
+
+const toggleMode = document.querySelector("#toggle-mode")
+const body = document.body
+toggleMode.addEventListener("click" , () => {
+  body.style.backgroundColor = "black";
+})
+ 
